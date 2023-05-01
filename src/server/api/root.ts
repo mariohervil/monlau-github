4aaa9b01396e94d59ea51b22b72693e2101ab076
mariohervil <mariohervil@campus.monlau.com>
2023-05-01 16:12:49 +0200

    routers set

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,13 +1,14 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
-
+import { userRouter } from "~/server/api/routers/user";
+import {projectRouter} from '~/server/api/routers/project'
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  users: userRouter,
+  projects: projectRouter
 });
 
 // export type definition of API

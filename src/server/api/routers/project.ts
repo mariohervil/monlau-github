3c12221fa7cb6461687b657f14e2af65417219c0
mariohervil <mariohervil@campus.monlau.com>
2023-05-01 16:12:24 +0200

    User + projects router

---
+++ src/server/api/routers/project.ts
@@ -0,0 +1,8 @@
+import { clerkClient, sessions } from "@clerk/nextjs/dist/api";
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const projectRouter = createTRPCRouter({
+  
+});

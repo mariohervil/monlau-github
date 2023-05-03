7a45e4c4c55128c11bc4a4cc510ce5b5bf766f09
mariohervil <mariohervil@campus.monlau.com>
2023-05-03 22:57:49 +0200

    Preparación user profile

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,25 @@
+import {
+  SignIn,
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  useUser,
+} from "@clerk/nextjs";
+import { api } from "~/utils/api";
+import { type z } from "zod";
+import Link from "next/link";
+import { type Project } from "~/pages/projects";
+import { type NextComponentType, type NextPage } from "next";
+
+const ProjectUploadWizard: NextComponentType = () => {
+  const { user } = useUser();
+
+  return <></>;
+};
+
+const UserProfile: NextPage = () => {
+  const user = useUser();
+
+  /* En las dobles comillas debe ir la id del usuario que se está viendo */
+  return <>{user.isSignedIn && user?.user.id === ""}</>;
+};
